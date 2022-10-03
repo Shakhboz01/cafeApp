c98b30929de703f7f87e254aad37f663c31dadfe
Shakhboz <shfazliddinov1940@gmail.com>
2022-10-03 16:04:53 +0500

    ap

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -6,14 +6,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APPS_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-  databaseURL: REACT_APP_DATABASE_URL
+  apiKey: process.env.local.REACT_APP_API_KEY,
+  authDomain: process.env.local.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.local.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.local.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.local.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.local.REACT_APP_APPS_ID,
+  measurementId: process.env.local.REACT_APP_MEASUREMENT_ID,
+  databaseURL: process.env.local.REACT_APP_DATABASE_URL
 };
 
 // Initialize Firebase
